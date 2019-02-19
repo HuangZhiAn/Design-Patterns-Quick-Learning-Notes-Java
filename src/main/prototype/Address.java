2a51c2d1a57bd1f5235aaaabc8913a8868cd539d
黄志安 <zhianhuang@outlook.com.com>
2019-02-19 15:13:27 +0800

    添加原型模式示例代码

---
+++ src/main/prototype/Address.java
@@ -0,0 +1,32 @@
+package prototype;
+
+import java.io.Serializable;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class Address implements Serializable {
+    private String province;
+    private String city;
+
+    public Address() {}
+
+    public Address(String province, String city) {
+        this.province = province;
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
